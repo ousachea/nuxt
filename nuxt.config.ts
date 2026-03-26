bbf77edd92cab2a7d3785ca8299b697fe125759d
ousachea <ousauser@gmail.com>
2026-03-26 11:38:31 +0700

    asfasf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000 // You can change this if needed
+  },
   devtools: { enabled: false },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
